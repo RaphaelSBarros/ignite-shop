78ff3229f7fe4e5ec8cdfe37c667460874622e12
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-10-18 13:31:57 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,29 @@
-import { styled } from '../../styles'
+import Image from 'next/image'
+import { HomeContainer, Product } from '../../styles/pages/home'
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,89</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 79,89</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
