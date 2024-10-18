7a82f6b2c65a94681d3c29555417345a6afdd83f
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-10-18 15:56:00 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,79</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+          repellat porro culpa nemo ea quisquam vitae a natus quae laborum
+          earum, voluptas nisi! Illum quisquam dolores eaque excepturi
+          consectetur repellendus.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
